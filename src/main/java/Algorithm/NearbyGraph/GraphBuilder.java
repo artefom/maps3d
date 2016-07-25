56b2e9c5b62e3ce28a8d762cd4ab24df90e800ed
Artyom.Fomenko <artefom@yandex.ru>
2016-07-25 23:02:20 +0300

    Added nearby line detection, graph building

---
+++ src/main/java/Algorithm/NearbyGraph/GraphBuilder.java
@@ -0,0 +1,7 @@
+package Algorithm.GraphBuilder;
+
+/**
+ * Created by Artyom.Fomenko on 25.07.2016.
+ */
+public class GraphBuilder {
+}
---
+++ src/main/java/Algorithm/NearbyGraph/Isoline_attributed.java
@@ -0,0 +1,7 @@
+package Algorithm.NearbyDetection;
+
+/**
+ * Created by Artyom.Fomenko on 25.07.2016.
+ */
+public class Isoline_attributed {
+}
