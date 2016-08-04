6338a04aaebae1253ea2100037f6e5a1c18fc277
fyodor.listvin <fyodor.listvin@jetbrains.com>
2016-08-04 17:24:25 +0300

    added flow parameter for skipping simplification

--- src/main/java/Utils/DebugUtils.java
+++ src/main/java/Utils/DebugUtils.java
@@ -6,4 +6,5 @@ package Utils;
 public class DebugUtils {
     // Isoline id counter. (constructor of isoline increments this value by id and uses new value as isoline ID)
     public static int isoline_last_id = -1;
+    public static boolean skipExternalSimplification = false;
 }
